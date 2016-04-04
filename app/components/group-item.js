0ffe47115f8d52c6670968a74c6504440339b267
unknown <yam@pc-yam.csia.net>
2016-04-04 13:17:17 +0300

    w

--- app/components/group-item.js
+++ app/components/group-item.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    actions: {
+        
+    addCard(group){
+        var store = this.get('targetObject.store');
+        let card = store.createRecord('card', {
+            text: "text, text, text, text",
+            type: 1,
+            group:group
+        });
+        card.save();
+        group.save();
+      },
+      
+      deleteCard(card){
+        card.destroyRecord();
+      }
+    }
 });
