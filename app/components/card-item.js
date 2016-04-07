e75fb28b659b2a9e7e513380fbb93de25053a9ef
MarianYakubets <marian.yakubets@gmail.com>
2016-04-07 21:38:13 +0300

    h

--- app/components/card-item.js
+++ app/components/card-item.js
@@ -2,18 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     hower:false,
-    
+
     mouseEnter: function(evt) {
         this.set('hower', true);
     },
-    
+
     mouseLeave: function(evt) {
        this.set('hower', false);
     },
-    
+
     actions : {
-        remove(params) {
-            console.log('remove');
+        remove(card) {
+            card.destroyRecord();
         }
     }
 });
