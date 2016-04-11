8579806baa0687c30b0bac55dcf90dbf9b88d7c5
MarianYakubets <marian.yakubets@gmail.com>
2016-04-11 21:57:19 +0300

    h

--- app/components/group-item.js
+++ app/components/group-item.js
@@ -1,25 +1,37 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    actions: {
+  newCardCreation: false,
+  newCardName:"",
 
-    addCard(group){
+  actions: {
+
+    addCard(){
+      this.set('newCardCreation', true);
+    },
+
+    createCardAction(group){
+      let newCardName = this.get("newCardName");
+      if(newCardName !== ""){
         var store = this.get('targetObject.store');
         let card = store.createRecord('card', {
-            text: "",
-            type: 1,
-            group:group
+          text: newCardName,
+          type: 1,
+          group: group
         });
         card.save();
         group.save();
-      },
+      }
+      this.set('newCardCreation', false);
+      this.set('newCardName', '');
+    },
 
-      deleteCard(card){
-        card.destroyRecord();
-      },
-      
-       saveAction(item){
-         item.save();
-      } 
+    deleteCard(card){
+      card.destroyRecord();
+    },
+
+    saveAction(item){
+      item.save();
     }
+  }
 });
